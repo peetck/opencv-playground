f0392e57dab377af3064aeb3e33898079267e0cd
peetck <peekung602@gmail.com>
2019-12-14 04:05:08 +0700

    Update read_write_show_images

--- read_write_show_images/main.py
+++ read_write_show_images/main.py
@@ -1,1 +1,10 @@
 import cv2
+
+def main():
+    """ main function """
+    image = cv2.imread("images/space.jpg", 1) # read space.jpg image file
+    cv2.imshow("Space Image", image) # display image(space.jpg)
+    cv2.imwrite("images/new_space.jpg", image) # copy space.jpg and save it as new_space.jpg
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+main()
