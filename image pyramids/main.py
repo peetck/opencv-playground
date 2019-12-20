8bf531693631b78257e93bc549d5813938441e94
peetck <peekung602@gmail.com>
2019-12-21 00:21:55 +0700

    Update image pyramids

--- image pyramids/main.py
+++ image pyramids/main.py
@@ -1,16 +1,35 @@
 import cv2
 import numpy as np
 
+"""
+Pyramid, or pyramid representation, is a type of multi-scale signal representation
+in which a signal or an image is subject to repeated smoothing and subsampling.
+"""
+
 def image_pyramids_example():
     """ image_pyramids_example function """
-    img = cv2.imread("chicky_512.png")
+    img = cv2.imread("lena-1.jpg") # read lena-1.jpg
+    cv2.imshow("Original Image", img) # display original image
 
-    layer = img.copy()
-    #gaussian_pyramid = [layer]
-    for i in range(5):
-        cv2.imshow(str(i + 1), layer)
-        layer = cv2.pyrDown(layer)
+    layer = img.copy() # copy img
+    gaussian_pyramid = [layer] # create gaussian pyramid list
+
+    for i in range(6):
+        layer = cv2.pyrDown(layer) # pyramid down current layer
+        gaussian_pyramid.append(layer) # append it to gaussian pyramid list
+        cv2.imshow("GP " + str(i), layer) # display after pyramid down current layer
+
+    layer = gaussian_pyramid[5] # set layer to last index of gaussian pyramid (upper level of gaussian pyramid)
+
+    # A level in Laplacian pyramid is formed by the difference between that level in Gaussian Pyramid and
+    # expanded version of its upper level in Gaussian Pyramid
+
+    for i in range(4, -1, -1):
+        gaussian_extended = cv2.pyrUp(gaussian_pyramid[i + 1]) # pyramid up current level
+        laplacian = cv2.subtract(gaussian_pyramid[i], gaussian_extended) #  difference between current level and upper level
+        cv2.imshow("LP " + str(i), laplacian) # display
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 image_pyramids_example()
