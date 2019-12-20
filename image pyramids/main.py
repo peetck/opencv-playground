9fe868daccf1fe3b150269264464797f337937f6
peetck <peekung602@gmail.com>
2019-12-20 23:53:00 +0700

    Update image pyramids

--- image pyramids/main.py
+++ image pyramids/main.py
@@ -5,7 +5,12 @@ def image_pyramids_example():
     """ image_pyramids_example function """
     img = cv2.imread("chicky_512.png")
 
-    cv2.imshow("Image", img)
+    layer = img.copy()
+    #gaussian_pyramid = [layer]
+    for i in range(5):
+        cv2.imshow(str(i + 1), layer)
+        layer = cv2.pyrDown(layer)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 image_pyramids_example()
